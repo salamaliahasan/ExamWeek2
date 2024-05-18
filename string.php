87a9e4045aa2410ec1f0104cf72d4ba41e58dc09
SalamAliAhasan <salamaliahasanbd@gmail.com>
2024-05-18 12:46:07 +0600

    String solution

--- string.php
+++ string.php
@@ -1,1 +1,12 @@
 <?php 
+
+function countVowelsRegex($string) {
+    preg_match_all('/[aeiouAEIOU]/', $string, $matches);
+    return count($matches[0]);
+}
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+for($i=0;$i<count($strings);$i++){
+    echo "Original String: $strings[$i], Vowel Count:" .countVowelsRegex($strings[$i]).", Reversed String: ".strrev($strings[$i])."\n";
+}
